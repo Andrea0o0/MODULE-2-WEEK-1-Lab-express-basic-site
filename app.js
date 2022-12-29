288405f62acd4d749dd2072180ae7e2ec2a0e05d
Andrea0o0 <andreagarciagonzalez22@gmail.com>
2022-12-29 17:02:59 +0100

    Layout Done

--- app.js
+++ app.js
@@ -3,16 +3,26 @@ const app = express()
 
 //MAKE FOLDER PUBLIC AVAILABLE
 app.use(express.static('public'))
+//HBS
+app.set('view engine','hbs')
+//VIEWS HBS
+app.set('views',__dirname + '/views')
+//LAYOUT
+// hbs.registerPartials(__dirname + '/views/partials')
 
 //HOME ROUTE
-app.get('/home',(request,response) => response.send('<h1>Welcome /home :)</h1>'))
+app.get('/',(request,response) => response.status(200).render('home'))
 
 //ABOUT ROUTE
-app.get('/about',(request,response) => response.send('<h1>Welcome /about :)</h1>'))
+app.get('/about',(request,response) => response.status(200).render('about'))
 
-//CONTACT ROUTE
-app.get('/contact',(request,response) => response.send('<h1>Welcome /contact :)</h1>'))
+//WORKS ROUTE
+app.get('/works',(request,response) => response.status(200).render('works'))
+
+//A PHOTO GALLERY ROUTE
+app.get('/photo-gallery',(request,response) => response.status(200).render('photo_gallery'))
 
 //SERVER STARTED
 app.listen(3000,() => console.log('My first app listening on port 3000!'))
+
 
